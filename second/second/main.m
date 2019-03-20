0991ee004de03d765f828b2ab8afa0f0012a59fa
YuraUlasevich <yura.ulasevich@gmail.com>
2019-03-20 12:42:26 +0300

    New project

---
+++ second/second/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  second
+//
+//  Created by Юра Уласевич on 3/19/19.
+//  Copyright © 2019 Юра Уласевич. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
